545926ef5c7ea14162790097121233467279da8f
Kushagra Kukreti <kukretikushagra@gmail.com>
2025-09-16 15:54:15 +0530

    add responsiveness

--- front-end/src/components/MainChatSection.tsx
+++ front-end/src/components/MainChatSection.tsx
@@ -3,10 +3,7 @@ import "../css/MainChatSection.css"
 
 const MainChatSection = () => {
   return (
-    <div className="main-chat-section" style={{display:"flex", justifyContent:"center"}}>
-        {/* <span className="col-4 sidebar-div"><SideBar/></span> */}
-        <span className="col-8"><ChatPage/></span>
-    </div>
+     <ChatPage/>  
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,55 +1,48 @@
-
-const http  =  require("http");
+const http = require("http");
 const express = require("express");
 const cors = require("cors");
 const socketIO = require("socket.io");
 
+const app = express(); // express start krdi
 
-const app = express() // express start krdi
+app.use(cors()); // cors use hoga inter-communication ke liye
+app.get("/", (req, res) => {
+  res.send("Hello from server bhaiji");
+});
 
-app.use(cors())  // cors use hoga inter-communication ke liye
-app.get("/",(req,res)=>{
-    res.send("Hello from server bhaiji");
-})
+const server = http.createServer(app); // humare server me app daal diya
 
-const server = http.createServer(app) // humare server me app daal diya
+const serverSocket = socketIO(server); // har socket ko server mile
 
-const serverSocket = socketIO(server) // har socket ko server mile
+serverSocket.on("connect", (clientSocketOnServerSide) => {
+  // serverSocket main circuit h  clientSocket ek user h
 
-serverSocket.on("connect",(clientSocketOnServerSide)=>{    // serverSocket main circuit h  clientSocket ek user h 
+  console.log("server connected");
 
-  console.log("server connected")
+  clientSocketOnServerSide.on("chatJoin", ({ dummyData }) => {
+    console.log("Received on server:", dummyData);
 
-  clientSocketOnServerSide.on("chatJoin",({dummyData})=>{   //chatJoin apna naam de skte h but firr client aur server side event ka naam same dena
-     console.log("Received on server:",dummyData)
+    const dummyDataFromServer = "This is coming from server side socket";
 
-     const dummyDataFromServer = "This is coming from server side socket"
+    clientSocketOnServerSide.emit("Welcome", { dummyDataFromServer });
 
-     clientSocketOnServerSide.emit("Welcome",{dummyDataFromServer})
-    
-     const msgForBroadcast = "Goes to everyone except the sender and the new socket ";
-     clientSocketOnServerSide.broadcast.emit("broadcast", {msgForBroadcast})
+    const msgForBroadcast =
+      "Goes to everyone except the sender and the new socket ";
+    clientSocketOnServerSide.broadcast.emit("broadcast", { msgForBroadcast });
+  });
 
-  })
+  clientSocketOnServerSide.on("message", ({ id, message, name }) => {
+    serverSocket.emit("sendMessage", { id, message, name });
+  });
 
-
-  clientSocketOnServerSide.on("message", ({id,message,name})=>{
-        serverSocket.emit("sendMessage",{id,message,name})
-  })
-
-
-  clientSocketOnServerSide.on("offline",()=>{
-    console.log("A user disconnected....")
-    const userLeftInfo = "A user left the chat room"
-    clientSocketOnServerSide.broadcast.emit("left",{userLeftInfo})
-  })
-
-  
-
-
-})
+  clientSocketOnServerSide.on("offline", () => {
+    console.log("A user disconnected....");
+    const userLeftInfo = "A user left the chat room";
+    clientSocketOnServerSide.broadcast.emit("left", { userLeftInfo });
+  });
+});
 
 const port = 8000 || process.env.PORT;
-server.listen(port,()=>{
-    console.log("Server started at:",port)
-})
+server.listen(port, () => {
+  console.log("Server started at:", port);
+});
